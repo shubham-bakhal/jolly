91faa97e38196ececf2378c64ee4ccb1755337f5
manish-basargekar <manish-basargekar@azodha.com>
2023-09-26 18:02:30 +0530

    update script

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,22 @@
-import  Style from "../styles/globals.css";
-
+import Style from "../styles/globals.css";
+import Script from "next/script";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<>
-			<Head>
-				<title>Jolly</title>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<meta
-					name="description"
-					content="Jolly is a fitness and wellness platform for everyone. Join us and get fit."
-				/>
-			</Head>
-			<Component {...pageProps} />
-		</>
-	);
+  return (
+    <>
+      <Head>
+	 
+        <title>Jolly</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Jolly is a fitness and wellness platform for everyone. Join us and get fit."
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from 'next/script'
+
 
 
 class MyDocument extends Document {
@@ -7,11 +7,10 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-				<Script
-					onError={(err) => console.log(err)}
-					onReady={() => console.log("is ready")}
-					strategy="lazyOnload"
-					type="module" id="annotate-site-webapp-script" defer src="https://cdn.jsdelivr.net/gh/devVSdesign/webapp-script-dev@v.1.0.2/index.js?bId=6512c6187ae6ece170f7bc81" />
+				<script
+					async
+					// defer
+					type="module" id="annotate-site-webapp-script" src="https://cdn.jsdelivr.net/gh/devVSdesign/webapp-script-dev@v.1.0.2/index.js?bid=6512c6187ae6ece170f7bc81"></script>
 					<link
 						href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,300,400&display=swap"
 						rel="stylesheet"
